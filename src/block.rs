0fa22709a46ad30efcedda665f77d674d8634fc8
Julian Pokrovsky <juliankul@gmail.com>
2021-07-11 12:36:21 +0300

    update test for block and little refactoring

--- src/block.rs
+++ src/block.rs
@@ -2,7 +2,7 @@ use std::time::SystemTime;
 
 use sha2::{Digest, Sha256};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Block {
     pub timestamp : SystemTime,
     pub last_hash: Vec<u8>,
@@ -29,19 +29,20 @@ impl Block {
     pub fn mine_block(last_block: Block, data: u8) -> Block {
         let now = SystemTime::now();
         let last_hash = last_block.hash;
+        let hash = {
+            let millis = now
+                .duration_since(std::time::UNIX_EPOCH)
+                .expect("Time went backwards")
+                .as_millis()
+                .to_string();
 
-        let since_the_epoch = now
-            .duration_since(std::time::UNIX_EPOCH)
-            .expect("Time went backwards")
-            .as_millis()
-            .to_string();
-
-        let hash = Sha256::new()
-            .chain(since_the_epoch.as_bytes())
-            .chain(last_hash.clone())
-            .chain([data])
-            .finalize()
-            .to_vec();
+            Sha256::new()
+                .chain(millis.as_bytes())
+                .chain(last_hash.clone())
+                .chain([data])
+                .finalize()
+                .to_vec()
+        };
 
         Block::new(now, last_hash, hash, 0)
     }
@@ -50,12 +51,12 @@ impl Block {
 
 #[cfg(test)]
 mod tests {
-    use std::time::SystemTime;
     use super::Block;
 
     #[test]
     fn test_block_creation() {
-        let block = Block::new(SystemTime::now(),0,0,0);
-        assert_eq!(0, block.data);
+        let genesis = Block::genesis();
+        let block = Block::mine_block(genesis.clone(), 9);
+        assert_eq!(genesis.hash, block.last_hash);
     }
 }
