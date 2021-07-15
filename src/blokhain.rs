823b4bf94718912f37c58feac7c8cae4af1bc596
Julian Pokrovsky <juliankul@gmail.com>
2021-07-15 11:21:03 +0300

    constructor

--- src/blokhain.rs
+++ src/blokhain.rs
@@ -3,3 +3,11 @@ use crate::block::Block;
 struct Blokhain {
     chain: Vec<Block>
 }
+
+impl Blokhain {
+    fn new() -> Self {
+        Blokhain {
+            chain: [Block::genesis()].to_vec()
+        }
+    }
+}
