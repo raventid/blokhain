3975f8e3644e7f05536d8fee0612ea5545601ec1
Julian Pokrovsky <juliankul@gmail.com>
2021-07-16 10:19:19 +0300

    add add_block func

--- src/blokhain.rs
+++ src/blokhain.rs
@@ -10,4 +10,9 @@ impl Blokhain {
             chain: [Block::genesis()].to_vec()
         }
     }
+
+    fn add_block(&mut self, data: u8) {
+        let block = Block::mine_block(self.chain.last().expect("genesis block is always here").clone(), data);
+        self.chain.push(block);
+    }
 }
